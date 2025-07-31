22541105002e22e6d4eaad52daa6ba597a5165ee
Akrit Kumar Behera <akbehera@akrit-cern-pc.dyndns.cern.ch>
2025-07-31 09:48:11 +0200

    Updated dcap to use custom autotools

--- dcap.sh
+++ dcap.sh
@@ -1,14 +1,18 @@
 package: dcap
-version: "%(tag_basename)s"
-tag: cms/2.47.12
-source: https://github.com/cms-externals/dcap
+version: "2.47.12"
+tag: 5753eec777a47908a40de670094903ce6b13176b
+sources:
+-  git+https://github.com/cms-externals/dcap.git?obj=cms/%(version)s/%(tag_basename)s&export=dcap-%(version)s&output=/dcap-%(version)s-%(tag_basename)s.tgz
+build_requires:
+- autotools
 requires:
 - zlib
 - gcc
 - gmake
 ---
-set -e
-rsync -a --chmod=ug=rwX --delete --exclude '**/.git' "$SOURCEDIR"/ "$BUILDDIR"/
+tar -xzf "$SOURCEDIR/${SOURCE0}" \
+    --strip-components=1 \
+    -C "$BUILDDIR" 
 if [ -f src/Makefile.am ]; then
     perl -p -i.bak -e 's|^library_includedir.*|library_includedir=\$(includedir)|' src/Makefile.am
     echo "Patched src/Makefile.am"
@@ -17,7 +21,7 @@ else
     echo "Warning: src/Makefile.am not found"
 fi
 mkdir -p config
-aclocal -I config
+aclocal -I config -I "$LIBTOOL_ROOT/share/aclocal"
 autoheader
 libtoolize --automake
 automake --add-missing --copy --foreign

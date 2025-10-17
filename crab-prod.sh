f62175799c247e1ccd4394538fae31ef8f26a066
Akrit Kumar Behera <akbehera@akrit-cern-pc.dyndns.cern.ch>
2025-10-17 16:12:50 +0200

    Oct 17 all crab

---
+++ crab-prod.sh
@@ -0,0 +1,17 @@
+package: crab-prod
+version: vCMS_prod
+variables:
+  version_suffix:     "00"
+  crabclient_version: "v3.250820"
+  crabserver_version: "v3.250818"
+  crabserver_packages: ""
+sources:
+  - git://github.com/dmwm/CRABClient.git?obj=master/%(crabclient_version)s&export=CRABClient&output=/CRABClient-%(crabclient_version)s.tar.gz
+  - git://github.com/dmwm/CRABServer.git?obj=master/%(crabserver_version)s&export=CRABServer&output=/CRABServer-%(crabserver_version)s.tar.gz
+---
+if [ -n "%(crabserver_packages)s" ]; then
+  export crabserver_packages=%(crabserver_packages)s
+else
+  export crabserver_packages="ServerUtilities.py"
+fi
+source crab-build.sh
--- demo.sh
+++ demo.sh
@@ -3,7 +3,12 @@ version: 6.4.0
 requires:
  - Python
  - gcc
+ - rpm
 ---
+echo $DEMO_HASH
+echo $PYTHON_VERSION
+echo $PYTHON_HASH
 
+echo $FULL_RUNTIME_REQUIRES
 
 

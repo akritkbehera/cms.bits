daaf0532075202092b692c55a86c5223d9c62cc4
Akrit Kumar Behera <akbehera@akrit-cern-pc.dyndns.cern.ch>
2025-10-29 13:30:14 +0100

    Added frontier_client and updated xz

--- autotools.sh
+++ autotools.sh
@@ -1,5 +1,5 @@
 package: autotools
-version: "v1.5"
+version: "1.5"
 variables:
  m4_version: "1.4.19"
  autoconf_version: "2.72"
@@ -14,9 +14,6 @@ sources:
  - https://mirror.ibcp.fr/pub/gnu/libtool/libtool-%(libtool_version)s.tar.gz
  - https://mirror.ibcp.fr/pub/gnu/gettext/gettext-%(gettext_version)s.tar.gz
  - https://pkgconfig.freedesktop.org/releases/pkg-config-%(pkg_config_version)s.tar.gz
-requires:
-- gcc
-
 env:
  M4: "$M4_ROOT/bin/m4"
 ---
@@ -26,71 +23,68 @@ for f in "$SOURCEDIR"/*; do
     esac
 done
 
-
-
 pushd $BUILDDIR/m4-%(m4_version)s
-./configure --disable-dependency-tracking --prefix="$INSTALLROOT"
-make ${JOBS:+-j$JOBS}
-make install
+  ./configure --disable-dependency-tracking --prefix="$INSTALLROOT"
+  make ${JOBS:+-j$JOBS}
+  make install
 popd
 
 pushd $BUILDDIR/autoconf-%(autoconf_version)s
-./configure --disable-dependency-tracking --prefix $INSTALLROOT
-make ${JOBS:+-j$JOBS} 
-make install
+  ./configure --disable-dependency-tracking --prefix="$INSTALLROOT"
+  make ${JOBS:+-j$JOBS}
+  make install
 popd
 
 pushd $BUILDDIR/automake-%(automake_version)s
-./configure --disable-dependency-tracking --prefix $INSTALLROOT
-make ${JOBS:+-j$JOBS} 
-make install
+  ./configure --disable-dependency-tracking --prefix="$INSTALLROOT"
+  make ${JOBS:+-j$JOBS}
+  make install
 popd
 
 pushd $BUILDDIR/libtool-%(libtool_version)s
-./configure --disable-dependency-tracking --prefix $INSTALLROOT  --enable-ltdl-install
-make ${JOBS:+-j$JOBS} 
-make install
+  ./configure --disable-dependency-tracking --prefix="$INSTALLROOT"  --enable-ltdl-install
+  make ${JOBS:+-j$JOBS}
+  make install
 popd
 
 pushd $BUILDDIR/gettext-%(gettext_version)s
-./configure \
-  --prefix=$INSTALLROOT \
-  --disable-dependency-tracking \
-  --disable-silent-rules \
-  --disable-rpath \
-  --disable-nls \
-  --disable-acl \
-  --disable-curses \
-  --disable-openmp \
-  --disable-native-java \
-  --disable-java \
-  --enable-relocatable \
-  --without-xz \
-  --without-bzip2 \
-  --with-included-libxml \
-  --with-included-glib \
-  --with-included-libunistring \
-  --with-included-libcroco
+  ./configure \
+    --prefix="$INSTALLROOT" \
+    --disable-dependency-tracking \
+    --disable-silent-rules \
+    --disable-rpath \
+    --disable-nls \
+    --disable-acl \
+    --disable-curses \
+    --disable-openmp \
+    --disable-native-java \
+    --disable-java \
+    --enable-relocatable \
+    --without-xz \
+    --without-bzip2 \
+    --with-included-libxml \
+    --with-included-glib \
+    --with-included-libunistring \
+    --with-included-libcroco
 
-make ${JOBS:+-j$JOBS} 
-make install
+  make ${JOBS:+-j$JOBS} 
+  make install
 popd
 
 pushd $BUILDDIR/pkg-config-%(pkg_config_version)s 
-./configure \
-  --prefix=$INSTALLROOT \
-  --disable-dependency-tracking \
-  --disable-silent-rules \
-  --disable-host-tool \
-  --disable-shared \
-  --with-internal-glib
-
-make ${JOBS:+-j$JOBS} 
-make install
+  ./configure \
+    --prefix="$INSTALLROOT" \
+    --disable-dependency-tracking \
+    --disable-silent-rules \
+    --disable-host-tool \
+    --disable-shared \
+    --with-internal-glib
+  
+  make ${JOBS:+-j$JOBS} 
+  make install
 popd
 
-grep -l -R '/bin/perl' $INSTALLROOT | xargs -n1 sed -ideleteme -e 's;^#!.*perl;#!/usr/bin/perl;'
+#grep -l -R '/bin/perl' $INSTALLROOT | xargs -n1 sed -ideleteme -e 's;^#!.*perl;#!/usr/bin/perl;'
 find $INSTALLROOT -name '*deleteme' -delete
-grep -l -R '/bin/perl' $INSTALLROOT | xargs -n1 sed -ideleteme -e 's;exec [^ ]*/perl;exec /usr/bin/perl;g'
+#grep -l -R '/bin/perl' $INSTALLROOT | xargs -n1 sed -ideleteme -e 's;exec [^ ]*/perl;exec /usr/bin/perl;g'
 find $INSTALLROOT -name '*deleteme' -delete
-

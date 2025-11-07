e7a1e72fdfeb883cb7720239b061493c858460e2
Akrit Kumar Behera <akbehera@akrit-cern-pc.dyndns.cern.ch>
2025-11-07 12:05:05 +0100

    cms-common

--- cms-common.sh
+++ cms-common.sh
@@ -1,11 +1,41 @@
 package: cms-common
-version: "1.0"
+version: vCMS
+tag: a9321840d2b6bdefae0376d0aa8236e650290b19
+source: https://github.com/cms-sw/cms-common
 variables:
-  tag: ac5bdc65fd10fdcc5b52cf855b7c67c92bf8627e
-sources:
- - git+https://github.com/cms-sw/cms-common.git?obj=master/%(tag)s&export=%(package)s-%=(version)s-%(tag)s&output=/%(package)s-%(version)s-%(tag)s.tgz
+  revision: "1256"
 ---
-tar -xzf "$SOURCEDIR/${SOURCE0}" \
-    --strip-components=1 \
-    -C "$BUILDDIR"
+export PKGVERSION="%(revision)s"
+rsync -a --chmod=ug=rwX --delete --exclude '**/.git' "$SOURCEDIR"/ "$BUILDDIR"/
+find . -type f -print0 | xargs -0 sed -i -e "s|@CMS_PREFIX@|${BITS_WORK_DIR}|g; s|@SCRAM_ARCH@|${ARCHITECTURE}|g"
+mkdir -p $INSTALLROOT/$PKGVERSION
+rsync $BUILDDIR/ $INSTALLROOT/
 
+cat > "$INSTALLROOT/etc/profile.d/post-relocate.sh" <<EoF
+#!/bin/bash -e
+mkdir -p $WORK_DIR/etc/$PKGNAME
+mkdir -p $WORK_DIR/$ARCHITECTURE/etc/profile.d
+if [ -f $WORK_DIR/cmsset_default.csh ] && [ -f $WORK_DIR/etc/$PKGNAME/revision ] ; then
+  oldrev=`cat $WORK_DIR/etc/$PKGNAME/revision`
+  if [ $oldrev -ge $PKGVERSION ] ; then
+    exit 0
+  fi
+fi
+mkdir -p $WORK_DIR/share 
+mkdir -p $WORK_DIR/etc/scramrc
+mkdir -p $WORK_DIR/common
+[ -d $BUILDDIR/etc/scramrc/SCRAM ] && rsync -a --delete $BUILDDIR/etc/scramrc/SCRAM/ $WORK_DIR/etc/scramrc/SCRAM/
+[ -d $BUILDDIR/share ] && rsync -a $BUILDDIR/share/ $WORK_DIR/share/
+pushd $BUILDDIR
+  files=\$(find . -type f -name '*' | grep -v '^./etc/scramrc/SCRAM' | sed 's|^\./||')
+  while IFS= read -r file; do
+    if [ -d "\$file" ] ; then
+      mkdir -p "$WORK_DIR/\$file"
+    else
+      rm -f "$WORK_DIR/\$file"
+      cp -P "\$file" "$WORK_DIR/\$file"
+     fi
+  done <<< "\$files"
+popd
+echo $PKGVERSION > $WORK_DIR/etc/$PKGNAME/revision
+EoF

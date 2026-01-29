8d84ab92ef9a20ec8456bb84e1411abc333e45c6
akritkbehera <akrit.kumar.behera@cern.ch>
2026-01-29 15:20:25 +0100

    cms-common

--- cms-common.sh
+++ cms-common.sh
@@ -4,48 +4,36 @@ tag: a9321840d2b6bdefae0376d0aa8236e650290b19
 source: https://github.com/cms-sw/cms-common
 variables:
   revision: "1256"
+env:
+  REVISION: "%(revision)s"
 ---
-export PKGVERSION="%(revision)s"
 rsync -a --chmod=ug=rwX --delete --exclude '**/.git' "$SOURCEDIR"/ "$BUILDDIR"/
-export files="$(find . -type f -name '*' | grep -v '^./etc/scramrc/SCRAM' | sed 's|^\./||')"
-echo $files
-mkdir -p $INSTALLROOT/$PKGVERSION
-rsync -a $BUILDDIR/ $INSTALLROOT/
-pushd $INSTALLROOT
-  find . -type f -exec sed -i -e "s|@CMS_PREFIX@|$WORK_DIR|g" -e "s|/lcg/|/|g" -e "s|/external/|/|g" {} +
-popd
-cat > "$INSTALLROOT/etc/profile.d/post-relocate.sh" <<EoF
-#!/bin/bash -e
-mkdir -p \$WORK_DIR/etc/$PKGNAME
-mkdir -p \$WORK_DIR/$ARCHITECTURE/etc/profile.d
 
-if [ -f \$WORK_DIR/cmsset_default.csh ] && [ -f \$WORK_DIR/etc/$PKGNAME/revision ] ; then
-  oldrev=\$(cat \$WORK_DIR/etc/$PKGNAME/revision)
-  if [ \$oldrev -gt \$PKGVERSION ] ; then
+find . -type f | xargs sed -i -e "s|@CMS_PREFIX@|$INSTALLROOT|g;s|@SCRAM_ARCH@|{$ARCHITECTURE}|"
+rsync -a $BUILDDIR/ $INSTALLROOT/
+
+cat > "$INSTALLROOT/etc/profile.d/post-relocate.sh" <<EoF
+if [ -f \$WORK_DIR/cmsset_default.csh ] && [ -f \$WORK_DIR/etc/cms-common/revision ] ; then
+  existing_rev=`cat \$WORK_DIR/etc/cms-common/revision`
+  if [ \$existing_rev -ge "%(revision)s" ] ; then
     exit 0
   fi
 fi
 
-mkdir -p \$WORK_DIR/share/profile.d
-mkdir -p \$WORK_DIR/etc/scramrc
-mkdir -p \$WORK_DIR/common
+mkdir -p \$WORK_DIR/etc/cms-common  \$WORK_DIR/\$ARCHITECTURE/etc/profile.d
 
-[ -d \$WORK_DIR/\$PP/etc/scramrc/SCRAM ] && rsync -a --delete \$WORK_DIR/\$PP/etc/scramrc/SCRAM/ \$WORK_DIR/etc/scramrc/SCRAM/
-[ -d \$WORK_DIR/\$PP/share ] && rsync -a \$WORK_DIR/\$PP/share/ \$WORK_DIR/share/
+mkdir -p \$WORK_DIR/share \$WORK_DIR/etc/scramrc
+  [ -d \$WORK_DIR/\$ARCHITECTURE/\$PKGNAME/\$PKGVERSION-\$PKGREVISION/share/etc/scramrc/SCRAM ] && rsync -a --delete \$WORK_DIR/\$ARCHITECTURE/\$PKGNAME/\$PKGVERSION-\$PKGREVISION/share/etc/scramrc/SCRAM/ \$WORK_DIR/etc/scramrc/SCRAM/
+  [ -d \$WORK_DIR/\$ARCHITECTURE/\$PKGNAME/\$PKGVERSION-\$PKGREVISION/share/share ] && rsync -a \$WORK_DIR/\$ARCHITECTURE/\$PKGNAME/\$PKGVERSION-\$PKGREVISION/share/ \$WORK_DIR/share/
 
-pushd \$WORK_DIR/\$PP
-
-files="$files"
-
-while IFS= read -r file; do
-  if [ -d "\$file" ] ; then
-    mkdir -p "\$WORK_DIR/\$file"
-  else
-    rm -f "\$WORK_DIR/\$file"
-    cp -P "\$file" "\$WORK_DIR/\$file"
-  fi
-done <<< "\$files"
-
+pushd \$WORK_DIR/\$ARCHITECTURE/\$PKGNAME/\$PKGVERSION-\$PKGREVISION
+ for file in \$(find . -name '*' | grep -v ./etc/scramrc/SCRAM ); do
+   if [ -d \$file ] ; then
+     mkdir -p \$WORK_DIR/\$file
+   else
+     rm -f \$WORK_DIR/\$file
+     cp -P \$file \$WORK_DIR/\$file
+    fi
+ done
 popd
-echo $PKGVERSION > \$WORK_DIR/etc/$PKGNAME/revision
-EoF
+  echo "%(revision)s" > \$WORK_DIR/etc/cms-common/revision

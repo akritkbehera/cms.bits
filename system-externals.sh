10145554f2444bcd2cccc6c6d013bf43d309a546
akritkbehera <akrit.kumar.behera@cern.ch>
2026-09-08 12:19:55 +0200

    Update system-externals to generate provides at deploy time

--- system-externals.sh
+++ system-externals.sh
@@ -1,20 +1,15 @@
 package: system-externals
 version: "1"
+sources: 
+ - file://system-externals.spec.in
 variables:
  seeds: 'bash glibc glibc-headers openssl-libs libX11 libxcrypt readline ncurses-libs tcl tk mesa-libGLU libglvnd-glx libglvnd-opengl libXext libXft libXpm perl perl-libs libbrotli python3 perl-base perl-lib perl-filetest perl-overload perl-vars libcom_err krb5-libs libaio libgcc'
+hooks: disable
 ---
 mkdir -p $INSTALLROOT/etc/rpm
 
-rpmbuild -bb \
-  --define "pkgname $PKGNAME" \
-  --define "pkgversion $PKGVERSION" \
-  --define "pkghash $PKGHASH" \
-  --define "tree $INSTALLROOT" \
-  --define "seeds %(seeds)s" \
-  --define "_rpmdir $INSTALLROOT/etc/rpm" \
-  --define "arch $(uname -m)" \
-  --define "_build_name_fmt %%{NAME}-%%{VERSION}.%%{ARCH}.rpm" \
-  $BITS_CONFIG_DIR/system-externals.spec.in
+cp $SOURCEDIR/system-externals.spec.in $INSTALLROOT
+touch $INSTALLROOT/system-externals.spec
 
 cat << 'EoF' > "$INSTALLROOT/etc/profile.d/post-relocate.sh"
 to_json() {
@@ -27,5 +22,18 @@ to_json() {
 EoF
 
 cat << EoF >> "$INSTALLROOT/etc/profile.d/post-relocate.sh"
-rpm -q --provides "\$WORK_DIR/\$PP/etc/rpm/$PKGNAME-${PKGVERSION}_$PKGHASH.$(uname -i).rpm" | to_json > "\$WORK_DIR/$ARCHITECTURE/system-provides.json"
+rpmspec -P \
+  --define "pkgname $PKGNAME" \
+  --define "pkgversion $PKGVERSION" \
+  --define "pkghash $PKGHASH" \
+  --define "tree $INSTALLROOT" \
+  --define "seeds %(seeds)s" \
+  --define "_rpmdir $INSTALLROOT/etc/rpm" \
+  --define "arch $(uname -m)" \
+  --define "_build_name_fmt %%{NAME}-%%{VERSION}.%%{ARCH}.rpm" \
+  \$WORK_DIR/\$PP/system-externals.spec.in > \$WORK_DIR/\$PP/system-externals.spec
+EoF
+
+cat << EoF >> "$INSTALLROOT/etc/profile.d/post-relocate.sh"
+rpmspec -q --provides "\$WORK_DIR/\$PP/system-externals.spec" | to_json > "\$WORK_DIR/$ARCHITECTURE/system-provides.json"
 EoF
